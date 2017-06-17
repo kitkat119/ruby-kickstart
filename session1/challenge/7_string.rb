2f4e7120bd2813b1686cea7d7e9850b970371653
kitkat119 <gprydal@yahoo.co.uk>
2017-06-17 01:36:44 +0000

    Session 1: challenge 7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -5,4 +5,11 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+   return_string = ""
+    string.length.times do |index|
+        if string[index] == "r"|| string[index] == "R" 
+          return_string << string[index+1].to_s
+        end
+   end
+    return_string
 end
