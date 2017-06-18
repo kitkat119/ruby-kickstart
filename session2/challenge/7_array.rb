24af2348c5f3e123f2ef7ee99c0391984342c5d3
kitkat119 <gprydal@yahoo.co.uk>
2017-06-18 16:54:29 +0000

    Session 2:Challenge 7

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -8,3 +8,8 @@
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(sentence)
+    '!@$#%^&*()-=_+[]:;,./<>?\\|'.split(//).each {|char| sentence = sentence.gsub(char, ' ')}
+    sentence.split.select.with_index { |word, index| index.even? }
+end
