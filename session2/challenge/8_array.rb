5c20373067a8992b92a00c416482932401ffb6d2
kitkat119 <gprydal@yahoo.co.uk>
2017-06-18 18:02:49 +0000

    Session 2: Challenge 8

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -5,3 +5,10 @@
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(arr)
+    new_arr = []
+    arr.each_cons(3) {|a| new_arr << a }
+    is_it_true = new_arr.map {|x| x.uniq.count == 1 }
+    is_it_true.include? (true)
+end
