3e417d224f19dc1d8292022876dc8906135b1fd0
kitkat119 <gprydal@yahoo.co.uk>
2017-06-19 14:03:07 +0000

    Session 3: Challenge 4

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,16 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(string)
+   
+    hash = {}
+    array = string.split
+    array.each_with_index do |word, index| 
+        hash[word] ||= index                    
+    end
+    hash
+end
+
+# ||= only assigns the index value if that particular word key hasn't already got a value,
+# checks if value on left is defined. If not then assigns the value on the right to it. 
